6a9d02e8658d93f0963619cffed1beeb65217385
nikunjbos26@gmail.com <your_email@example.com>
2026-01-23 19:49:17 +0530

    Added load_data.py, changed logic in read_data.py and inserted load_data stage in dvc.yaml

---
+++ src/load_data.py
@@ -0,0 +1,25 @@
+from read_data import read_csv
+import argparse
+
+def load_save_data(parameters_path: str):
+
+    # Load data using read_csv function
+    df = read_csv(parameters_path)
+    print("Data loaded successfully")
+
+    # There are spaces between column names, removing them.
+    # For example: 'fixed acidity, 'volitile acidity' -> 'fixed_acidity', 'volitile_acidity'
+    df.columns = [col.replace(" ", "_") for col in df.columns]
+    print("Columns renamed successfully")
+
+    # Save the dataset to data/raw
+    raw_data_path = r"data/raw/raw_winequality.csv"
+    df.to_csv(raw_data_path, index = False)
+
+    print(f"Data saved to {raw_data_path} successfully")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--parameters_path", default="parameters.yaml")
+    args = parser.parse_args()
+    load_save_data(parameters_path = args.parameters_path)
--- src/read_data.py
+++ src/read_data.py
@@ -6,15 +6,20 @@ import yaml
 import pandas as pd
 import argparse
 
+# Defining a function to read yaml file.
+def read_yaml(parameters_path: str):
+    with open(parameters_path, 'r') as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+    return yaml_data
+
 # Defining a function to read yaml file, get csv file path and read csv file
 
-def read_data(parameters_path: str)-> pd.DataFrame:
+def read_csv(parameters_path: str)-> pd.DataFrame:
 
     # Read parameters.yaml file
-    with open(parameters_path) as yaml_file:
-        data = yaml.safe_load(yaml_file)
-    
-    csv_file_path = data['data_source']['local_data_path']
+    yaml_data = read_yaml(parameters_path)
+
+    csv_file_path = yaml_data['data_source']['local_data_path']
 
     # Read csv file
     df = pd.read_csv(csv_file_path)
@@ -27,5 +32,5 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--parameters_path", default = "parameters.yaml")
     parsed_args = args.parse_args()
-    data = read_data(parameters_path = parsed_args.parameters_path)
+    data = read_csv(parameters_path = parsed_args.parameters_path)
     print(data.head())
